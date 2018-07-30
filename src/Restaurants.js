d5078d2e0d946e2c047226c45dd3126b7e878526
Himanshu Patel <himanshup@csu.fullerton.edu>
2018-07-29 18:33:57 -0700

    connected to firebase

--- src/Restaurants.js
+++ src/Restaurants.js
@@ -12,77 +12,62 @@ import {
   CardSubtitle,
   Badge
 } from "reactstrap";
+import {db} from './firebase'
 
 class Restaurants extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name:"",
+    };
+  }
+
   componentDidMount() {
     document.title = "Restaurants";
+
+    this.restaurantCollection = db.collection("restaurants").onSnapshot(collection => {
+      const courses = collection.docs.map(doc => doc.data())
+      this.setState({courses})
+    });
+
+    this.menuCollection = db.collection("/restaurants/3WYzeaPJLiCJPCIG2r8g/menu").onSnapshot(collection => {
+      const menus = collection.docs.map(doc => doc.data())
+      this.setState({menus})
+    });
   }
 
   render() {
     return (
       <Container>
         <Row>
-          <Col xs="12" sm="12" md="6" lg="4">
+        {this.state.courses && this.state.courses.map((topic, index) =>
+          <Col xs="12" sm="12" md="6" lg="4" key={index}>
             <Link to="/order" className="custom-card">
               <Card className="shadow border-0 hvr-float">
                 <CardImg
                   top
                   width="100%"
-                  src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-                  alt="name"
+                  src={topic.picture}
+                  alt={topic.name}
                 />
                 <CardBody>
-                  <CardTitle>Restaurant Name (Address)</CardTitle>
+                  <CardTitle>{topic.name} ({topic.address})</CardTitle>
                   <CardSubtitle />
                   <CardText>
-                    <Badge color="secondary">Food</Badge>
-                    <Badge color="secondary">Type</Badge>
-                    <Badge color="secondary">Here</Badge>
+                    <Badge color="secondary">{topic.tags[0]}</Badge>
+                    <Badge color="secondary">{topic.tags[1]}</Badge>
+                    <Badge color="secondary">{topic.tags[2]}</Badge>
                   </CardText>
                 </CardBody>
               </Card>
             </Link>
           </Col>
-          <Col xs="12" sm="12" md="6" lg="4">
-            <Link to="/order" className="custom-card">
-              <Card className="shadow border-0 hvr-float">
-                <CardImg
-                  top
-                  width="100%"
-                  src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-                  alt="name"
-                />
-                <CardBody>
-                  <CardTitle>Restaurant Name (Address)</CardTitle>
-                  <CardSubtitle />
-                  <CardText>
-                    <Badge color="secondary">Something</Badge>
-                    <Badge color="secondary">Here</Badge>
-                  </CardText>
-                </CardBody>
-              </Card>
-            </Link>
-          </Col>
-          <Col xs="12" sm="12" md="6" lg="4">
-            <Link to="/order" className="custom-card">
-              <Card className="shadow border-0 hvr-float">
-                <CardImg
-                  top
-                  width="100%"
-                  src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-                  alt="name"
-                />
-                <CardBody>
-                  <CardTitle>Restaurant Name (Address)</CardTitle>
-                  <CardSubtitle />
-                  <CardText>
-                    <Badge color="secondary">Something</Badge>
-                  </CardText>
-                </CardBody>
-              </Card>
-            </Link>
-          </Col>
+        )}
         </Row>
+        <h1>menu</h1>
+        <p>
+        {this.state.menus &&  this.state.menus.map((topic, index) =><li key={index}>{topic.name}<br />Size: {topic.size}<br />Side: {topic.side}<br />Drink: {topic.drink}</li>)}
+        </p>
       </Container>
     );
   }
--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,12 @@
 import firebase from "firebase";
 require("firebase/firestore");
-// Initialize Firebase
 var config = {
-  apiKey: "AIzaSyBBTjIzadGBj-rS-dKhmitma_n5xVQqS08",
-  authDomain: "test-3a209.firebaseapp.com",
-  databaseURL: "https://test-3a209.firebaseio.com",
-  projectId: "test-3a209",
-  storageBucket: "",
-  messagingSenderId: "82637738644"
+  apiKey: "AIzaSyBznKu6OeZL39BP6HdiZkglyC2TVAvk60k",
+  authDomain: "project-2-4244a.firebaseapp.com",
+  databaseURL: "https://project-2-4244a.firebaseio.com",
+  projectId: "project-2-4244a",
+  storageBucket: "project-2-4244a.appspot.com",
+  messagingSenderId: "1028123459499"
 };
 firebase.initializeApp(config);
 
